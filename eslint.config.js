8c8965c46965e946b1f81b5ca246718be1cc1374
azarnuzy <azarnuzy@gmail.com>
2024-09-06 15:42:34 +0700

    feat: initialization project cakra.ai with tailwind configuration, eslint, and prettier

---
+++ eslint.config.js
@@ -0,0 +1,87 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+export default tseslint.config(
+  { ignores: ['dist', 'node_modules'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'unused-imports': unusedImports,
+      'simple-import-sort': simpleImportSort
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-unused-vars': 'off',
+      'no-console': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'react/no-unescaped-entities': 'off',
+
+      'react/display-name': 'off',
+
+      //#region  //*=========== Unused Import ===========
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'warn',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_'
+        }
+      ],
+      //#endregion  //*======== Unused Import ===========
+
+      //#region  //*=========== Import Sort ===========
+      'simple-import-sort/exports': 'warn',
+      'simple-import-sort/imports': [
+        'warn',
+        {
+          groups: [
+            // ext library & side effect imports
+            ['^@?\\w', '^\\u0000'],
+            // {s}css files
+            ['^.+\\.s?css$'],
+            // Lib and hooks
+            ['^@/lib', '^@/hooks'],
+            // static data
+            ['^@/data'],
+            // components
+            ['^@/components', '^@/container'],
+            // zustand store
+            ['^@/store'],
+            // Other imports
+            ['^@/'],
+            // relative paths up until 3 level
+            [
+              '^\\./?$',
+              '^\\.(?!/?$)',
+              '^\\.\\./?$',
+              '^\\.\\.(?!/?$)',
+              '^\\.\\./\\.\\./?$',
+              '^\\.\\./\\.\\.(?!/?$)',
+              '^\\.\\./\\.\\./\\.\\./?$',
+              '^\\.\\./\\.\\./\\.\\.(?!/?$)'
+            ],
+            ['^@/types'],
+            // other that didnt fit in
+            ['^']
+          ]
+        }
+      ]
+    }
+  }
+);
