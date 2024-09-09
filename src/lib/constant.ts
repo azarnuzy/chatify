67a92a4057f009e30879af7c7e949ae6ffe1b309
azarnuzy <azarnuzy@gmail.com>
2024-09-09 11:46:04 +0700

    fix: update docker

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const SECRET_KEY = import.meta.env.VITE_SECRET_KEY_OPEN_AI;
+export const SECRET_KEY = import.meta.env.VITE_SECRET_KEY_GEMINI_AI;
