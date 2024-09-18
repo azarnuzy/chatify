79a5382281b57f67e6cda86c29f149f0b3082ecf
azarnuzy <azarnuzy@gmail.com>
2024-09-18 07:12:08 +0700

    fix: update .env.example and fix link privacy and terms in auth layout

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -63,11 +63,11 @@ export default function AuthLayout({
 
             <p className="px-8 text-center text-sm text-muted-foreground">
               By clicking continue, you agree to our{' '}
-              <Link to="/terms" className="underline underline-offset-4 hover:text-primary">
+              <Link to="" className="underline underline-offset-4 hover:text-primary">
                 Terms of Service
               </Link>{' '}
               and{' '}
-              <Link to="/privacy" className="underline underline-offset-4 hover:text-primary">
+              <Link to="" className="underline underline-offset-4 hover:text-primary">
                 Privacy Policy
               </Link>
               .
